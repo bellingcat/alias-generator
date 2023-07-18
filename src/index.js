7b136b45eabc6ab06d7616f7a0e9ac8b9cc26a44
lilia <liliakai@gmail.com>
2023-07-18 14:10:08 +0200

    MVP implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-const getAliases = (nameStr) => [nameStr];
+const humanparser = require('humanparser');
+
+const getAliases = (nameStr) => {
+  let name = humanparser(nameStr);
+  /*
+    {
+        salutation: 'Mr.',
+        firstName: 'William',
+        suffix: 'III',
+        lastName: 'Hearst',
+        middleName: 'R.',
+        fullName: 'Mr. William R. Hearst, III'
+    }
+  */
+  [
+    name.fullName,
+    [name.firstName, name.lastName].join(' '),
+  ]
+}
 
 export default getAliases;
