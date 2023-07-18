10b636d4cd2f9e4c3ed1c46de475851a471ad263
lilia <liliakai@gmail.com>
2023-07-18 17:07:24 +0200

    Add some more variants with middle name/initials

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import humanparser from 'humanparser';
 
+const getMiddleInitial = (nameObj) => {
+  if (!nameObj.middleName || typeof nameObj.middleName !== 'string') {
+    return null;
+  }
+
+  return nameObj.middleName[0].toUpperCase();
+}
+
 const getAliases = (nameStr) => {
   let name = humanparser.parseName(nameStr);
+  name.middleInitial = getMiddleInitial(name);
+
   /*
     {
         salutation: 'Mr.',
@@ -12,10 +22,23 @@ const getAliases = (nameStr) => {
         fullName: 'Mr. William R. Hearst, III'
     }
   */
-  return [
-    name.fullName,
+  let results = [
     [name.firstName, name.lastName].join(' '),
+    [name.lastName, name.firstName].join(', '),
   ];
+  if (name.middleName) {
+    results.push(
+      [name.firstName, name.middleName, name.lastName].join(' '),
+      [name.lastName, [name.firstName, name.middleName].join(' ')].join(', ') ,
+    );
+  }
+  if (name.middleInitial) {
+    results.push(
+      [name.firstName, name.middleInitial, name.lastName].join(' '),
+      [name.lastName, [name.firstName, name.middleInitial].join(' ')].join(', ') ,
+    );
+  }
+  return results;
 }
 
 export default getAliases;
